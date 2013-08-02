11b51ea88b681a5bd2726e77970878ddecd90d7e
Roy Ganor <ganoro@gmail.com>
2013-08-02 15:02:31 +0300

    add add platforms?

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,40 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.AudioHandler/www/MediaError.js",
+        "id": "org.apache.cordova.core.AudioHandler.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.AudioHandler/www/Media.js",
+        "id": "org.apache.cordova.core.AudioHandler.Media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.device/www/device.js",
+        "id": "org.apache.cordova.core.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.network-information/www/network.js",
+        "id": "org.apache.cordova.core.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.network-information/www/Connection.js",
+        "id": "org.apache.cordova.core.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    }
+]
+});
